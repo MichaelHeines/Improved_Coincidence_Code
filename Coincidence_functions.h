65284c33d5732a37cc172319e1a03a8b64e8d6b4
MichaelHeines <84977146+MichaelHeines@users.noreply.github.com>
2022-02-18 14:17:38 +0100

    Add files via upload

---
+++ Coincidence_functions.h
@@ -0,0 +1,178 @@
+#ifndef coincidencefunctions_H
+#define coincidencefunctions_H
+
+#include "File_chainer.h"
+#include <iostream>
+#include <vector>
+
+
+void coincidence_creation(std::vector<TString> coincidences_filename, TString this_dir, TString write_dir, Int_t nr_datapoints, ULong64_t window, Int_t cutted_events, Int_t window_events, TString data_type_chain, TString data_type_tree, Int_t hist_bins, Double_t ns_window) {
+	// Write a root file containing info on the coincidences 
+
+	// Chain the files
+	TChain* mychain = new TChain(data_type_chain);
+	add_files_to_chain(mychain, data_type_tree, ".root", this_dir);
+
+	// Prepare assignments input tree
+	TBranch* Energy_branch = mychain->GetBranch("Energy");							// Assign the branches
+	TBranch* Channel_branch = mychain->GetBranch("Channel");
+	TBranch* Time_branch = mychain->GetBranch("Timestamp");
+	UShort_t energy, channel;														// Define the datatypes of the leaves
+	ULong64_t time;
+	mychain->SetBranchAddress("Energy", &energy);									// Sets the branches to a variable
+	mychain->SetBranchAddress("Channel", &channel);
+	mychain->SetBranchAddress("Timestamp", &time);
+	const Int_t entry = mychain->GetEntries();
+	std::cout << entry << " Entries in chain" << std::endl;
+
+	// Setup coincidence tree
+	ULong64_t coinc_time;
+	Float_t t_diff;
+	UShort_t coinc_e0, coinc_e1, coinc_detector;
+	bool coinc_bool;
+	TString coinc_name = "Coincidence events ";
+
+	// Define/set some variables/parameters
+	ULong64_t t0, t1;
+	Float_t dt;
+	UShort_t e0, e1;
+	bool multiple;
+	Int_t nr_coincidences = 0;
+	Double_t range_low, range_high;
+
+	// Loop over number of desired data points within a batch
+	for (Double_t k = 0; k < nr_datapoints; k++) {
+		std::cout << coincidences_filename[k] << std::endl;
+		TFile* hfile = TFile::Open(write_dir + coincidences_filename[k], "RECREATE");
+		TTree* coinc_tree = new TTree("ctree", coinc_name);								// Make a new tree for the coincidence events
+		coinc_tree->Branch("coinc_time", &coinc_time, "coinc_time/L");					// Make a timestamp branch
+		coinc_tree->Branch("t_diff", &t_diff, "t_diff/F");								// Make a time difference branch
+		coinc_tree->Branch("coinc_detector", &coinc_detector, "coinc_detector/s");		// Make a detector branch
+		coinc_tree->Branch("coinc_e0", &coinc_e0, "coinc_e0/s");						// Make a trigger branch
+		coinc_tree->Branch("coinc_e1", &coinc_e1, "coinc_e1/s");						// Make a coincidence branch
+		coinc_tree->Branch("multiple_flag", &coinc_bool, "multiple flag/O");			// If a particle is not the first coincidence to a trigger
+
+		TH1F* hcoinc = new TH1F("hcoinc", "Energy spectrum", hist_bins, 0, hist_bins);			// Can be used to check present coincidence energies
+		TH1F* htdiff = new TH1F("htdiff", "Time difference", 40, -ns_window * 1000, ns_window * 1000);
+
+		range_low = cutted_events + round(k * entry / nr_datapoints);
+		range_high = round((k + 1) * entry / nr_datapoints) - cutted_events;
+
+		// Loop that finds coincidences and puts them in a new tree
+		for (Int_t i = range_low; i < range_high; i++) {								// Iterate over all events
+			mychain->GetEntry(i);
+			multiple = false;
+
+			if (channel == 0) {															// If the event is in detector 0: Check events in 1 detector and see if anything in the other one is in coincidence
+				t0 = time;
+				e0 = energy;
+
+				for (Int_t j = -window_events; j < window_events; j++) {				// Check window_events before and after
+					mychain->GetEntry(i + j);
+					if (channel == 1) {													// Only check second detector
+						t1 = time;
+						e1 = energy;
+
+						if (t1 < t0) {													// Make dt positive (for some reason you can't directly use value if it is negative
+							dt = t0 - t1;
+						}
+						else {
+							dt = t1 - t0;
+						}
+
+						if (dt <= window) {												// If in coincidence, add event to newly created tree
+							if (t1 < t0) {
+								dt *= -1;
+							}
+							nr_coincidences += 1;
+							hcoinc->Fill(energy);
+							htdiff->Fill(dt);
+
+							coinc_time = t0;											// Fill detector 0 in coincidence histogram
+							t_diff = dt;
+							coinc_detector = 0;
+							coinc_e0 = e0;
+							coinc_e1 = e1;
+							coinc_bool = multiple;
+							coinc_tree->Fill();
+
+							coinc_time = t1;											// Fill detector 1 in coincidence histogram
+							t_diff = -dt;
+							coinc_detector = 1;
+							coinc_e0 = e1;
+							coinc_e1 = e0;
+							coinc_bool = multiple;
+							coinc_tree->Fill();
+
+							multiple = true;											// On next iteration for same trigger, note there was already a coincidence
+						}
+					}
+				}
+			}
+		}
+		// Write (and potentially draw) the energy spectrum of coincidences
+		//hcoinc->Draw();
+		//htdiff->Draw();
+		hcoinc->Write("", TObject::kOverwrite);
+		htdiff->Write("", TObject::kOverwrite);
+
+		// Write (and potentially print) the coincidence tree
+		//coinc_tree->Print();
+		coinc_tree->Write("", TObject::kOverwrite);
+		hfile->TFile::Close("R");
+	}
+
+	std::cout << "Saved Coincidence trees" << std::endl;
+}
+
+
+
+std::vector<Int_t> get_coincrates(TString directory, TString filename, std::vector<Double_t> channels0, std::vector<Double_t> channels1, std::vector<Int_t> hwidth) {
+	TFile* f = new TFile(directory + filename);
+	TTree* mytree; f->GetObject("ctree", mytree);
+
+	Long64_t time;
+	Float_t tdiff;
+	UShort_t energy, energy_c, detector;
+	bool flag;
+	Int_t N01 = 0;
+	Int_t N10 = 0;
+
+	TBranch* coinc_time_branch = mytree->GetBranch("coinc_time");
+	TBranch* t_diff_branch = mytree->GetBranch("t_diff");
+	TBranch* coinc_det_branch = mytree->GetBranch("coinc_detector");
+	TBranch* coinc_e0_branch = mytree->GetBranch("coinc_e0");
+	TBranch* coinc_e1_branch = mytree->GetBranch("coinc_e1");
+	TBranch* mult_flag_branch = mytree->GetBranch("multiple_flag");
+	mytree->SetBranchAddress("coinc_time", &time);
+	mytree->SetBranchAddress("t_diff", &tdiff);
+	mytree->SetBranchAddress("coinc_detector", &detector);
+	mytree->SetBranchAddress("coinc_e0", &energy);
+	mytree->SetBranchAddress("coinc_e1", &energy_c);
+	mytree->SetBranchAddress("multiple_flag", &flag);
+
+	int nr_entries = mytree->GetEntries();
+	//std::cout << nr_entries << std::endl;
+	
+	for (int i = 0; i < nr_entries; i++) {
+		mytree->GetEntry(i);
+
+		if (detector == 0) {
+			if (abs(energy - channels0[0]) <= hwidth[0]) {
+				if (abs(energy_c - channels1[1]) <= hwidth[1]) {
+					N01 += 1;
+				}
+			}
+			else if (abs(energy - channels0[1]) <= hwidth[1]) {
+				if (abs(energy_c - channels1[0]) <= hwidth[0]) {
+					N10 += 1;
+				}
+			}
+		}
+	}
+	f->TFile::Close();
+
+	return { N01, N10 };
+}
+
+#endif
---
+++ Time_functions.h
@@ -0,0 +1,95 @@
+#ifndef timefunctions_H
+#define timefunctions_H
+
+#include <iostream>
+#include <vector>
+
+// For a batch
+std::vector<Double_t> get_meantimes(const Int_t nr_dirs, std::vector<Double_t> init_times, std::vector<Double_t> durations, Double_t half_life) {
+	std::vector<Double_t> mean_times(nr_dirs);
+	Double_t decay_constant = log(2) / half_life;
+
+	for (Int_t i = 0; i < nr_dirs; i++) {
+		mean_times[i] = init_times[i] + (durations[i] / 86400.0) + (1 / decay_constant) * log(decay_constant * durations[i] / 86400.0) - (1 / decay_constant) * log(exp(decay_constant * durations[i] / 86400.0) - 1);
+	}
+
+	return mean_times;
+}
+
+
+
+Double_t meas_time(TString read_dir, TString filename) {
+	TChain* mytree = new TChain("Data_R");
+	add_files_to_chain(mytree, "DataR", ".root", read_dir + filename);
+
+	TBranch* Time_branch = mytree->GetBranch("Timestamp");
+	ULong64_t time;
+	mytree->SetBranchAddress("Timestamp", &time);
+	const Int_t entry = mytree->GetEntries();									// Number of entries in the root file
+
+	Double_t measurement_time;														// Length of measurement
+	Double_t final_time;
+	Double_t initial_time;
+
+	mytree->GetEntry(1);
+	initial_time = time / 1.0e12;
+	mytree->GetEntry(entry - 1);
+	final_time = time / 1.0e12;
+	measurement_time = final_time - initial_time;
+	//std::cout << "Measured for " << measurement_time << std::endl;
+
+	return measurement_time;
+}
+
+
+
+// For a single file
+Double_t get_meantime(Double_t init_t, Double_t duration, Double_t half_life) {
+	Double_t mean_t;
+	Double_t decay_constant = log(2) / half_life;
+
+	mean_t = init_t + (duration / 86400.0) + (1 / decay_constant) * log(decay_constant * duration / 86400.0) - (1 / decay_constant) * log(exp(decay_constant * duration / 86400.0) - 1);
+
+	return mean_t;
+}
+
+
+
+Double_t start_time(TString write_dir, TString filename) {
+	TFile* f = new TFile(write_dir + filename);															// Load the root file
+	TTree* mytree; f->GetObject("ctree", mytree);
+	TBranch* coinc_time_branch = mytree->GetBranch("coinc_time");
+	Long64_t time;
+	mytree->SetBranchAddress("coinc_time", &time);
+
+	const Int_t entry = mytree->GetEntries();									// Number of entries in the root file
+	Double_t initial_time;
+	mytree->GetEntry(1);
+	initial_time = time / 1.0e12;
+
+	return initial_time;
+}
+
+
+
+Double_t meas_t(TString write_dir, TString filename) {
+	TFile* f = new TFile(write_dir + filename);															// Load the root file
+	TTree* mytree; f->GetObject("ctree", mytree);
+	TBranch* coinc_time_branch = mytree->GetBranch("coinc_time");
+	Long64_t time;
+	mytree->SetBranchAddress("coinc_time", &time);
+	const Int_t entry = mytree->GetEntries();									// Number of entries in the root file
+
+	Double_t measurement_time;														// Length of measurement
+	Double_t final_time;
+	Double_t initial_time;
+
+	mytree->GetEntry(1);
+	initial_time = time / 1.0e12;
+	mytree->GetEntry(entry - 1);
+	final_time = time / 1.0e12;
+	measurement_time = final_time - initial_time;
+
+	return measurement_time;
+}
+#endif
