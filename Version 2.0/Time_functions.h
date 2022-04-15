1fcc5954d2734fe6e170255cff9ad17cd8dc6f0a
MichaelHeines <84977146+MichaelHeines@users.noreply.github.com>
2022-04-15 23:34:08 +0200

    Update Time_functions.h

--- Version 2.0/Time_functions.h
+++ Version 2.0/Time_functions.h
@@ -7,7 +7,7 @@
 #include <TROOT.h>
 #include <TFile.h>
 
-// For a batch
+// Calculate the meantime for a number of directories
 std::vector<Double_t> get_meantimes(const Int_t nr_dirs, std::vector<Double_t> init_times, std::vector<Double_t> durations, Double_t half_life) {
 	std::vector<Double_t> mean_times(nr_dirs);
 	Double_t decay_constant = log(2) / half_life;
@@ -20,17 +20,17 @@ std::vector<Double_t> get_meantimes(const Int_t nr_dirs, std::vector<Double_t> i
 }
 
 
-
+// Determine the duration of the measurement
 Double_t meas_time(TString read_dir, TString filename, TString datatype_chain, TString datatype_tree) {
 	TChain* mytree = new TChain(datatype_chain);
-	add_files_to_chain(mytree, datatype_tree, ".root", read_dir + filename);
+	add_files_to_chain(mytree, datatype_tree, ".root", read_dir + filename);				// Get the relevant data in a chain
 
 	TBranch* Time_branch = mytree->GetBranch("Timestamp");
 	ULong64_t time;
 	mytree->SetBranchAddress("Timestamp", &time);
-	const Int_t entry = mytree->GetEntries();									// Number of entries in the root file
+	const Int_t entry = mytree->GetEntries();								// Number of entries in the root file
 
-	Double_t measurement_time;														// Length of measurement
+	Double_t measurement_time;										// Length of measurement
 	Double_t final_time;
 	Double_t initial_time;
 
@@ -46,7 +46,7 @@ Double_t meas_time(TString read_dir, TString filename, TString datatype_chain, T
 
 
 
-// For a single file
+// For a single file, calculate the meantime
 Double_t get_meantime(Double_t init_t, Double_t duration, Double_t half_life) {
 	Double_t mean_t;
 	Double_t decay_constant = log(2) / half_life;
@@ -57,15 +57,15 @@ Double_t get_meantime(Double_t init_t, Double_t duration, Double_t half_life) {
 }
 
 
-
+// Determine the start time of a single measurement
 Double_t start_time(TString write_dir, TString filename) {
-	TFile* f = new TFile(write_dir + filename);															// Load the root file
+	TFile* f = new TFile(write_dir + filename);								// Load the root file
 	TTree* mytree; f->GetObject("ctree", mytree);
 	TBranch* coinc_time_branch = mytree->GetBranch("coinc_time");
 	Long64_t time;
 	mytree->SetBranchAddress("coinc_time", &time);
 
-	const Int_t entry = mytree->GetEntries();									// Number of entries in the root file
+	const Int_t entry = mytree->GetEntries();								// Number of entries in the root file
 	Double_t initial_time;
 	mytree->GetEntry(1);
 	initial_time = time / 1.0e12;
@@ -74,16 +74,16 @@ Double_t start_time(TString write_dir, TString filename) {
 }
 
 
-
+// Determine the measurement time for a datapoint
 Double_t meas_t(TString write_dir, TString filename) {
-	TFile* f = new TFile(write_dir + filename);															// Load the root file
+	TFile* f = new TFile(write_dir + filename);								// Load the root file
 	TTree* mytree; f->GetObject("ctree", mytree);
 	TBranch* coinc_time_branch = mytree->GetBranch("coinc_time");
 	Long64_t time;
 	mytree->SetBranchAddress("coinc_time", &time);
-	const Int_t entry = mytree->GetEntries();									// Number of entries in the root file
+	const Int_t entry = mytree->GetEntries();								// Number of entries in the root file
 
-	Double_t measurement_time;														// Length of measurement
+	Double_t measurement_time;										// Length of measurement
 	Double_t final_time;
 	Double_t initial_time;
 
